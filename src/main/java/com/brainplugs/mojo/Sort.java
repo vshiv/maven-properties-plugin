c35800db6979b2c4495f160f83b57e0135ebefa8
Vinay Shivshankar <vinay.shivshankar@cerner.com>
2017-07-20 23:08:10 -0500

    Skip flag supported. Overwrite file only when modification to hash detected

--- src/main/java/com/brainplugs/mojo/Sort.java
+++ src/main/java/com/brainplugs/mojo/Sort.java
@@ -4,72 +4,58 @@ import org.apache.commons.io.FileUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Execute;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Collection;
-import java.util.Properties;
 
 /**
  * Mojo that sorts resource file keys
  */
-@Mojo (name = "sort-keys", defaultPhase = LifecyclePhase.PROCESS_RESOURCES, threadSafe = true)
+@Mojo (name = "sort-keys", defaultPhase = LifecyclePhase.COMPILE, threadSafe = true)
+@Execute(phase = LifecyclePhase.COMPILE)
 public class Sort extends AbstractMojo {
 
-    @Parameter (defaultValue = "${project.basedir}/src/main/resources", readonly = true)
+    @Parameter (property = "properties.root.dir", defaultValue = "${project.basedir}/src/main/resources", readonly = true)
     private File resourcesRoot;
 
-    @Parameter
+    @Parameter (property = "properties.file.ext")
     private String[] fileExtensions;
+
+
+    @Parameter( property = "properties.sort.skip", defaultValue = "false" )
+    private boolean skip;
 
     private static final String[] defaultFileExtensions = {"props", "properties"};
 
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("Resource root specified as : " + resourcesRoot.getAbsolutePath());
-        fileExtensions = (fileExtensions !=null && fileExtensions.length > 0) ? fileExtensions : defaultFileExtensions;
-        final Collection<File> files = FileUtils.listFiles(resourcesRoot, fileExtensions, true);
-        files.forEach( f -> {
-                        getLog().info("Sort keys for file : " + f.getName());
-                        readAndSort(f);
-                      });
+        if(skip){
+            getLog().info("Properties maven plugin execution skipped");
+            return;
+        }
+        if(resourcesRoot.exists()) {
+            getLog().info("Resource root specified as : " + resourcesRoot.getAbsolutePath());
+            fileExtensions = (fileExtensions != null && fileExtensions.length > 0) ? fileExtensions : defaultFileExtensions;
+            final Collection<File> files = FileUtils.listFiles(resourcesRoot, fileExtensions, true);
+            files.forEach(this::readAndSort);
+        }
     }
 
     private void readAndSort(final File file) {
-        InputStream input = null;
-        OutputStream output = null;
-        final Properties properties = new SortedProperties();
+        final SortedProperties properties = new SortedProperties();
         try {
-            input = new FileInputStream(file);
-            properties.load(input);
-            properties.entrySet().stream().forEach(entry -> getLog().debug("Key -> " + entry.getKey() + " Value -> " + entry.getValue() ));
-            input.close();
-            output = new FileOutputStream(file);
-            properties.store(output, null);
-            output.close();
+            final boolean overwrite = properties.load(file);
+            if(overwrite) {
+                getLog().info("Writing sorted keys to file :" + file.getName());
+                properties.entrySet().stream().forEach(entry -> getLog().debug("Key -> " + entry.getKey() + " Value -> " + entry.getValue()));
+                properties.store(file);
+            }
         } catch (final IOException fe) {
             getLog().error(fe);
-        } finally {
-            if (input != null) {
-                try {
-                    input.close();
-                } catch (final IOException ioe) {
-                    getLog().error(ioe);
-                }
-            }
-            if (output != null) {
-                try {
-                    output.close();
-                } catch (final IOException ioe) {
-                    getLog().error(ioe);
-                }
-            }
         }
     }
 }
--- src/main/java/com/brainplugs/mojo/SortedProperties.java
+++ src/main/java/com/brainplugs/mojo/SortedProperties.java
@@ -1,8 +1,16 @@
 package com.brainplugs.mojo;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Collections;
 import java.util.Enumeration;
 import java.util.List;
+import java.util.Optional;
 import java.util.Properties;
 
 /**
@@ -14,5 +22,30 @@ class SortedProperties extends Properties{
         final List<Object> keysList = Collections.list(super.keys());
         Collections.sort(keysList , (o1, o2 ) -> o1.toString().compareTo(o2.toString()));
         return Collections.enumeration(keysList);
+    }
+
+
+    boolean load(final File file) throws IOException {
+        final FileInputStream inputStream = FileUtils.openInputStream(file);
+        super.load(inputStream);
+        inputStream.close();
+        final List<String> lines = FileUtils.readLines(file);
+        final Optional<String> first = lines.stream().findFirst();
+        if(first.isPresent() && first.get().startsWith("#properties.maven.hash")){
+            final String hashLine = first.get();
+            final String[] lastHash = hashLine.split("=");
+            return lastHash.length == 2 && !lastHash[1].equalsIgnoreCase("" + this.hashCode());
+        }
+        return true;
+    }
+
+    void store(final File file) throws IOException {
+       final FileOutputStream fileOutputStream = new FileOutputStream(file);
+       final PrintWriter writer = new PrintWriter(fileOutputStream);
+       writer.println("#properties.maven.hash=" + hashCode());
+       writer.flush();
+       super.store(fileOutputStream, null);
+       writer.close();
+       fileOutputStream.close();
     }
 }
